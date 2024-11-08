d722c550f145f2b12b204669d435f45d1689a984
Jincheol Park <clearwater1219@gmail.com>
2024-11-08 19:58:09 +0100

    build: change layer order

--- add_layers.sh
+++ add_layers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-bitbake-layers add-layer ../meta-raspberrypi
-bitbake-layers add-layer ../meta-customimage
-bitbake-layers add-layer ../meta-qt6
-bitbake-layers add-layer ../meta-qttest
 bitbake-layers add-layer ../meta-openembedded/meta-oe
+bitbake-layers add-layer ../meta-qt6
+bitbake-layers add-layer ../meta-raspberrypi
+bitbake-layers add-layer ../meta-qttest
+bitbake-layers add-layer ../meta-customimage
 
